972dfc583cb6cfd80f2164147d65b195be51f2e7
meta-khushi-agrawal4 <52689075+meta-khushi-agrawal4@users.noreply.github.com>
2019-08-22 07:35:28 +0530

    Update session3Assignment5.sql

--- DBMSAssignment3/session3Assignment5.sql
+++ DBMSAssignment3/session3Assignment5.sql
@@ -1,10 +1,10 @@
 /* Assignment 5 Question 1 */
+
 create view orderInformation as
 select po.order_id, po.order_date, u.user_name, u.email_id, po.order_amount, p.product_name, o.product_status, o.product_id, o.quantity
-from user as u inner join user_order as uo 
-on u.user_id=uo.user_id
-left outer join product_order as po 
-on po.order_id=uo.order_id
+from user as u 
+inner join product_order as po 
+on po.user_id=u.user_id
 left outer join order_item as o
 on o.order_id=po.order_id
 left outer join product as p 
@@ -23,4 +23,5 @@ from orderInformation
 group by product_id
 order by sum(quantity) desc
 limit 5;
+ 
  
